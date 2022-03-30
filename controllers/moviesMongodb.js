af2a9672547b708dd9f9f3ec190a07a58596f6e9
albertorg93 <futberto1993@gmail.com>
2022-03-30 22:50:31 +0200

    se añade conexion a mongodb y se incluyen pelis de prueba

---
+++ controllers/moviesMongodb.js
@@ -0,0 +1,37 @@
+const paraNada = require('../utils/dbMongo')
+
+const Product = require("../models/moviesMongodb")
+
+
+const getProduct = async (req,res) => {
+    if (req.params.id) {
+        const product = await Product.find({id:req.params.id}); // Devuelve 1
+        res.status(200).json(product); // Pinta datos en el pug
+    } else {
+        const allProducts = await Product({});
+        res.status(200).json(allProducts); // Pinta datos en el pug
+    }
+}
+
+const createProduct = async (req,res) => {
+    console.log(req.body); // Objeto recibido de producto nuevo
+    const newProduct = req.body; // {} nuevo producto a guardar
+    // Líneas
+    //para guardar 
+    // en una BBDD SQL o MongoDB
+    const response = await Product.save(newProduct)
+ 
+
+    res.status(201).json({message: `Producto ${answer.title} guardado en el sistema con ID: ${answer.id}`});
+}
+
+//const editProduct = ...
+//const deleteProduct = ...
+
+const product = {
+    getProduct,
+    createProduct,
+    //editProduct,
+    //deleteProduct
+}
+module.exports = product;
