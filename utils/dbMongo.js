1210247efc887756f0374228cc10078c5af96789
albertorg93 <futberto1993@gmail.com>
2022-04-18 21:34:37 +0200

    deleted comments on utils/dbmongo

--- utils/dbMongo.js
+++ utils/dbMongo.js
@@ -1,7 +1,6 @@
 
 const mongoose = require("mongoose");
 const url = process.env.URL_MONGO;
-console.log(url,"esta es la url de mongo")
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on("error", error => console.log(error));
